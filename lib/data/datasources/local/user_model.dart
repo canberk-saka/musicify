1b1b7fce7aa26ae2af4b53db7b36c5a33b00b850
canberk-saka <ahmetcanberksaka@gmail.com>
2025-02-18 09:42:28 +0300

    Add Realm dependency and UserModel class for local data storage

---
+++ lib/data/datasources/local/user_model.dart
@@ -0,0 +1,19 @@
+// import 'package:realm/realm.dart';
+
+// part 'user_model.g.dart';
+
+// ///{@template userModel}
+// ///User Model
+// ///{@endtemplate}
+// @RealmModel()
+// class _UserModel {
+//   ///{@macro userModel}
+//   _UserModel({this.id, this.email, this.password});
+
+//   ///Kullanıcı id'si
+//   late String? id;
+// ///Kullanıcı email'i
+//   late String? email;
+//   ///Kullanıcı şifresi
+//   String? password;
+// }
